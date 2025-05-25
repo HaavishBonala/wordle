705894310d62f0d3a342cbb7ffb5ce3000468466
Haavish Bonala <76700308+HaavishBonala@users.noreply.github.com>
2025-05-25 08:18:45 +0100

    Add files via upload

---
+++ wordle.py
@@ -0,0 +1,72 @@
+"""
+The game should ask the user for their initial guess 
+
+The game should only allow 5 letter words to be entered 
+
+The game should check the user?s guess against the correct word 
+
+The game should indicate which letters are in the word in blue 
+
+The game should indicate which letters are in the correct place 
+
+The game should indicate when the user has guessed the correct word 
+
+If the user has not guessed the word in 6 goes they should be told they have not completed the game and told the correct word 
+"""
+
+import random
+import requests
+import colorama
+from colorama import Fore, Back, Style
+
+colorama.init(autoreset=True)
+
+def user_input():
+    while True:
+        x = input("enter your 5 letter guess(all letters only): ").lower()
+        if len(x) != 5:
+            print("your guess contains more than 5 charecter")
+            continue
+    
+        if not x.isalpha():
+            print("your guess contains a non-alphabetic charecter")
+            continue
+            
+        return x
+
+def get_random_word():
+    response = requests.get("https://scipython.com/static/media/uploads/blog/wordle/common-5-words.txt")
+    data = response.text
+    data = data.split()
+    return random.choice(data)
+
+def print_color(letter,col):
+    if col == "g":
+        print(Fore.GREEN + letter)
+    elif col == "y":
+        print(Fore.YELLOW + letter)
+    elif col == "r":
+        print(Fore.RED + letter)
+
+def check_what_contains_check_whatis(guess,answer):
+    for i in range(5):
+        if guess[i] == answer[i]:
+            #green
+            print_color(guess[i],"g")
+        elif guess[i] in answer:
+            #yellow
+            print_color(guess[i],"y")
+        else:
+            #red
+            print_color(guess[i],"r")
+
+print("Welcome to wordle")
+answer = get_random_word()
+
+for i in range(5):
+    print("no of attempts left:", i)
+    guess = user_input()
+    check_what_contains_check_whatis(guess,answer)
+    if guess == answer:
+        print("sucess")
+        break
